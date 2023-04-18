5ac8ef6ca9c2458322fb4b22ce49dbedf1c6080d
aleksandrcumakov <ach@fivegen.ru>
2023-04-18 11:02:22 +0300

    added allure

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import random
 import time
 import uuid
 
+import allure
 import pytest
 import requests
 
@@ -11,6 +12,7 @@ import mainfunc as mf
 
 # Получение результатов (GET/events) по панорамным скришотам
 @pytest.mark.panoramic
+@allure.title('Получение результатов (GET/events) по панорамным скришотам')
 def test_get_panoramic_events():
     panoramic_image_type = 1
     response = mf.get_events(c.IMAGE_TYPE, panoramic_image_type)
@@ -22,6 +24,7 @@ def test_get_panoramic_events():
 
 # Проверка отсутствия результатов обработки архивов из других источников
 @pytest.mark.panoramic
+@allure.title('Проверка отсутствия результатов обработки архивов из других источников')
 def test_check_for_results_from_another_sources():
     timestamp_list = []
     values = [0, 1, 2]
@@ -46,6 +49,7 @@ def test_check_for_results_from_another_sources():
 
 # Актуализация списка расписаний ЕЦХД
 @pytest.mark.zkh
+@allure.title('Актуализация списка расписаний ЕЦХД')
 def test_get_list_of_actual_schedules_ECHD():
     url = f"{c.host}/schedules/refresh"
     headers = c.AUTH_HEADERS
@@ -55,6 +59,7 @@ def test_get_list_of_actual_schedules_ECHD():
 
 
 # Создание задачи на обработку – проверка функционала ЖКХ
+@allure.title('Создание задачи на обработку – проверка функционала ЖКХ')
 @pytest.mark.zkh
 def test_create_task():
     response = mf.create_task(labels=c.RANDOM_LABELS(),
@@ -69,6 +74,7 @@ def test_create_task():
 @pytest.mark.zkh
 @pytest.mark.parametrize('labels', [random.random(), None, ['101']])
 @pytest.mark.parametrize('detectors', [random.random(), None, ['10']])
+@allure.title('Создание задачи с указанием некорректных параметров «labels»/«detectors»')
 def test_create_task_with_incorrect_label_or_detector(labels, detectors):
     if labels != None and detectors != None:
         response = mf.create_task(labels, detectors, c.SCHEDULE_ID)
@@ -77,6 +83,7 @@ def test_create_task_with_incorrect_label_or_detector(labels, detectors):
 
 # Создание задачи дублированной задачи (с идентичными «labels»/«detectors»)
 @pytest.mark.zkh
+@allure.title('Создание задачи дублированной задачи (с идентичными «labels»/«detectors»)')
 def test_create_task_with_existed_params():
     detectors = c.RANDOM_DETECTORS
     labels = c.RANDOM_LABELS()
@@ -93,6 +100,7 @@ def test_create_task_with_existed_params():
 
 # Создание задачи с несуществующим расписанием («schedule»)
 @pytest.mark.zkh
+@allure.title('Создание задачи с несуществующим расписанием («schedule»)')
 def test_create_task_with_non_existed_schedule():
     response = mf.create_task(c.RANDOM_LABELS(),
                               [random.randint(5, 20)], str(uuid.uuid4()))
@@ -102,6 +110,7 @@ def test_create_task_with_non_existed_schedule():
 
 # Создание задачи с некорректным расписанием («schedule»)
 @pytest.mark.zkh
+@allure.title('Создание задачи с некорректным расписанием («schedule»)')
 def test_create_task_with_incorrect_format_schedule():
     response = mf.create_task(c.RANDOM_LABELS(),
                               c.RANDOM_DETECTORS, str(random.random()))
@@ -111,6 +120,7 @@ def test_create_task_with_incorrect_format_schedule():
 
 # Создание задачи при отсутствии одного из обязательных параметров – без указания «schedule»)
 @pytest.mark.zkh
+@allure.title('Создание задачи при отсутствии одного из обязательных параметров – без указания «schedule»)')
 def test_create_task_without_schedule():
     response = mf.create_task(c.RANDOM_LABELS(), c.RANDOM_DETECTORS, None)
     assert response.status_code == 400
@@ -119,6 +129,7 @@ def test_create_task_without_schedule():
 
 # Создание задачи при отсутствии одного из обязательных параметров в запросе – пустой параметр «labels»
 @pytest.mark.zkh
+@allure.title('Создание задачи при отсутствии одного из обязательных параметров в запросе – пустой параметр «labels»')
 def test_create_task_without_label():
     response = mf.create_task(labels=None,
                               detectors=c.RANDOM_DETECTORS, schedule_id=c.SCHEDULE_ID)
@@ -130,6 +141,7 @@ def test_create_task_without_label():
 
 # Создание задачи при отсутствии обязательных параметров в запросе – пустые параметры «labels» и «detectors»
 @pytest.mark.zkh
+@allure.title('Создание задачи при отсутствии обязательных параметров в запросе – пустые параметры «labels» и «detectors»')
 def test_create_task_without_labels_and_detectors():
     response = mf.create_task(labels=None, detectors=None, schedule_id=c.SCHEDULE_ID)
     assert response.status_code == 400
@@ -138,6 +150,7 @@ def test_create_task_without_labels_and_detectors():
 
 # Получение списка созданных заданий - проверка наличия задачи в списке созданных заданий
 @pytest.mark.zkh
+@allure.title('Получение списка созданных заданий - проверка наличия задачи в списке созданных заданий')
 def test_create_and_get_task_from_tasks_list_show_cancelled_true():
     response = mf.create_task(c.RANDOM_LABELS(), c.RANDOM_DETECTORS, c.SCHEDULE_ID)
     task_id = response.json()[c.TASK_ID]
@@ -158,6 +171,7 @@ def test_create_and_get_task_from_tasks_list_show_cancelled_true():
 
 # Получение списка задач без параметра «showCancelled»
 @pytest.mark.zkh
+@allure.title('Получение списка задач без параметра «showCancelled»')
 def test_create_and_get_task_from_tasks_list_wothout_show_cancelled():
     response = mf.create_task(c.RANDOM_LABELS(), c.RANDOM_DETECTORS, c.SCHEDULE_ID)
     task_id = response.json()[c.TASK_ID]
@@ -178,6 +192,7 @@ def test_create_and_get_task_from_tasks_list_wothout_show_cancelled():
 
 # Получение списка всех задач
 @pytest.mark.zkh
+@allure.title('Получение списка всех задач')
 def test_get_tasks():
     response = mf.get_tasks()
     assert response.status_code == 200
@@ -186,6 +201,7 @@ def test_get_tasks():
 
 # Получение статуса задания
 @pytest.mark.zkh
+@allure.title('Получение статуса задания')
 def test_get_task_status():
     labels = c.RANDOM_LABELS()
     response = mf.create_task(labels, c.RANDOM_DETECTORS, c.SCHEDULE_ID)
@@ -201,6 +217,7 @@ def test_get_task_status():
 
 # Получение статуса задачи по несуществующему taskId
 @pytest.mark.zkh
+@allure.title('Получение статуса задачи по несуществующему taskId')
 def test_get_non_existed_task_status():
     response = mf.get_task_status(str(uuid.uuid4()))
     assert response.status_code == 404
@@ -209,6 +226,7 @@ def test_get_non_existed_task_status():
 
 # Получение статуса задачи с некорректным taskId(не соотвествует формату uuid)
 @pytest.mark.zkh
+@allure.title('Получение статуса задачи с некорректным taskId(не соотвествует формату uuid)')
 def test_get_wrong_format_task_status():
     response = mf.get_task_status(str(random.random()))
     assert response.status_code == 400
@@ -217,6 +235,7 @@ def test_get_wrong_format_task_status():
 
 # Получение результатов анализа по задаче
 @pytest.mark.zkh
+@allure.title('Получение результатов анализа по задаче')
 def test_get_results():
     response = mf.create_task(c.RANDOM_LABELS(), c.RANDOM_DETECTORS, c.SCHEDULE_ID)
     task_id = response.json()[c.TASK_ID]
@@ -230,6 +249,7 @@ def test_get_results():
 
 # Получение результатов анализа по задаче с некорректным taskId (не соответствует формату uuid)
 @pytest.mark.zkh
+@allure.title('Получение результатов анализа по задаче с некорректным taskId (не соответствует формату uuid)')
 def test_get_wrong_format_task_results():
     response = mf.get_task_result(str(random.random()))
     assert response.status_code == 400
@@ -238,6 +258,7 @@ def test_get_wrong_format_task_results():
 
 # Получение результатов анализа по задаче по несуществующему taskId
 @pytest.mark.zkh
+@allure.title('Получение результатов анализа по задаче по несуществующему taskId')
 def test_get_non_existed_task_results():
     response = mf.get_task_result(str(uuid.uuid4()))
     assert response.status_code == 404
@@ -246,6 +267,7 @@ def test_get_non_existed_task_results():
 
 # Приостановить задачу
 @pytest.mark.zkh
+@allure.title('Приостановить задачу')
 def test_stop_task():
     response = mf.create_task(c.RANDOM_LABELS(), c.RANDOM_DETECTORS, c.SCHEDULE_ID)
     task_id = response.json()[c.TASK_ID]
@@ -260,6 +282,7 @@ def test_stop_task():
 
 # Приостановить задачу, которая уже на паузе
 @pytest.mark.zkh
+@allure.title('Приостановить задачу, которая уже на паузе')
 def test_pause_already_paused_task():
     response = mf.create_task(c.RANDOM_LABELS(), c.RANDOM_DETECTORS, c.SCHEDULE_ID)
     task_id = response.json()[c.TASK_ID]
@@ -275,6 +298,7 @@ def test_pause_already_paused_task():
 
 # Изменение статуса задачи «cancelled» на «paused»
 @pytest.mark.zkh
+@allure.title('Изменение статуса задачи «cancelled» на «paused»')
 def test_pause_cancelled_task():
     task_id = mf.create_task(c.RANDOM_LABELS(), c.RANDOM_DETECTORS, c.SCHEDULE_ID).json()[c.TASK_ID]
     stop = mf.stop_task(task_id)
@@ -289,6 +313,7 @@ def test_pause_cancelled_task():
 
 # Возобновление задачи - изменение статуса задачи «paused» на «inProgress»
 @pytest.mark.zkh
+@allure.title('Возобновление задачи - изменение статуса задачи «paused» на «inProgress»')
 def test_resume_paused_task():
     task_id = mf.create_task(c.RANDOM_LABELS(), c.RANDOM_DETECTORS, c.SCHEDULE_ID).json()[c.TASK_ID]
     mf.pause_task(task_id)
@@ -300,6 +325,7 @@ def test_resume_paused_task():
 
 # Изменение статуса задачи «inProgress» на «inProgress»
 @pytest.mark.zkh
+@allure.title('Изменение статуса задачи «inProgress» на «inProgress»')
 def test_resume_resumed_task():
     task_id = mf.create_task(c.RANDOM_LABELS(), c.RANDOM_DETECTORS, c.SCHEDULE_ID).json()[c.TASK_ID]
     response = mf.resume_task(task_id)
@@ -312,6 +338,7 @@ def test_resume_resumed_task():
 
 # Изменение статуса задачи «cancelled» на «inProgress»
 @pytest.mark.zkh
+@allure.title('Изменение статуса задачи «cancelled» на «inProgress»')
 def test_resume_cancelled_task():
     task_id = mf.create_task(c.RANDOM_LABELS(), c.RANDOM_DETECTORS, c.SCHEDULE_ID).json()[c.TASK_ID]
     stop = mf.stop_task(task_id)
@@ -327,6 +354,7 @@ def test_resume_cancelled_task():
 
 # Отмена задачи - изменение статуса задачи «inProgress» на «cancelled»
 @pytest.mark.zkh
+@allure.title('Отмена задачи - изменение статуса задачи «inProgress» на «cancelled»')
 def test_cancel_task():
     task_id = mf.create_task(c.RANDOM_LABELS(), c.RANDOM_DETECTORS, c.SCHEDULE_ID).json()[c.TASK_ID]
     cancel = mf.stop_task(task_id)
@@ -339,6 +367,7 @@ def test_cancel_task():
 
 # Изменение статуса задачи «paused» на «cancelled»
 @pytest.mark.zkh
+@allure.title('Изменение статуса задачи «paused» на «cancelled»')
 def test_cancel_paused_task():
     task_id = mf.create_task(c.RANDOM_LABELS(), c.RANDOM_DETECTORS, c.SCHEDULE_ID).json()[c.TASK_ID]
     pause = mf.pause_task(task_id)
@@ -355,6 +384,7 @@ def test_cancel_paused_task():
 
 # Изменение статуса задачи «cancelled» на «cancelled»
 @pytest.mark.zkh
+@allure.title('Изменение статуса задачи «cancelled» на «cancelled»')
 def test_cancel_cancelled_task():
     task_id = mf.create_task(c.RANDOM_LABELS(), c.RANDOM_DETECTORS, c.SCHEDULE_ID).json()[c.TASK_ID]
     mf.stop_task(task_id)
@@ -368,6 +398,7 @@ def test_cancel_cancelled_task():
 
 # Отправка экспертной оценки по скриншоту
 @pytest.mark.zkh
+@allure.title('Отправка экспертной оценки по скриншоту')
 def test_send_expert_answer():
     screenshot_id = mf.get_screenshot_id_from_db()
     response = mf.send_expert_answer(screenshot_id)
@@ -379,6 +410,10 @@ def test_send_expert_answer():
 # Получение статуса задания
 # Получение результатов анализа по задаче, в поле "labels" получаем значение массива [71,81,91,101,111,121,131,141,151,161,171,181]
 @pytest.mark.zkh
+@allure.title('создание задачи, ЖКХ')
+@allure.description('Создание задачи на обработку, в поле "detectors" указывается значение массива [7,8,9,10,11,12,13,14,15,16,17,18]'
+                    'Получение статуса задания'
+                    'Получение результатов анализа по задаче, в поле "labels" получаем значение массива [71,81,91,101,111,121,131,141,151,161,171,181]')
 def test_create_zkh_task():
     labels = c.ZKH_LABELS
     detectors = c.ZKH_DETECTORS
@@ -403,6 +438,10 @@ def test_create_zkh_task():
 # Получение статуса задания
 # Получение результатов анализа по задаче, в поле "labels" получаем значение массива [191,201,211,221,231,241,251,261]
 @pytest.mark.zkh
+@allure.title('создание задачи, ЖКХ+')
+@allure.description('Создание задачи на обработку, в поле "detectors" указывается значение массива [19,20,21,22,23,24,25,26]'
+                    'Получение статуса задания'
+                    'Получение результатов анализа по задаче, в поле "labels" получаем значение массива [191,201,211,221,231,241,251,261]')
 def test_create_zkh_plus_task():
     labels = c.ZKH_PLUS_LABELS
     detectors = c.ZKH_PLUS_DETECTORS
@@ -427,6 +466,10 @@ def test_create_zkh_plus_task():
 # Получение статуса задания
 # Получение результатов анализа по задаче, в поле "labels" получаем значение массива [51,61]
 @pytest.mark.zkh
+@allure.title('создание задачи, ИНС МАГАЗИНЫ')
+@allure.description('Создание задачи на обработку, в поле "detectors" указывается значение массива [5,6]'
+                    'Получение статуса задания'
+                    'Получение результатов анализа по задаче, в поле "labels" получаем значение массива [51,61]')
 def test_create_zkh_shops_task():
     labels = c.ZKH_SHOPS_LABELS
     detectors = c.ZKH_SHOPS_DETECTORS
@@ -451,6 +494,10 @@ def test_create_zkh_shops_task():
 # Получение статуса задания
 # Получение результатов анализа по задаче, в поле "labels" получаем значение массива [271,281,291,301,311]
 @pytest.mark.zkh
+@allure.title('создание задачи, ЖКХ++')
+@allure.description('Создание задачи на обработку, в поле "detectors" указывается значение массива [27,28,29,30,31]'
+                    'Получение статуса задания'
+                    'Получение результатов анализа по задаче, в поле "labels" получаем значение массива [271,281,291,301,311]')
 def test_create_zkh_new_task():
     labels = c.ZKH_NEW_LABELS
     detectors = c.ZKH_NEW_DETECTORS
@@ -469,3 +516,4 @@ def test_create_zkh_new_task():
     assert type(result.json()[c.RESULTS]) == list
 
     mf.delete_task_from_db(task_id)
+
